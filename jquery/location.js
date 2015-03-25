d2d66a2f3b23130679cee11c87c61a680606ce66
angeladamaso <angeladamaso6@gmail.com>
2015-03-25 19:57:24 +0800

    OCMMA

---
+++ jquery/location.js
@@ -0,0 +1,686 @@
+var prk = [
+['Ulo ng Apo Monument', 14.838847, 120.284371],
+['Marikit Park', 14.837294, 120.283379],
+['Oval', 14.832751, 120.282670],
+['Rizal Triangle', 14.8416643, 120.28705620000005],
+['Olongapo City Museum', 14.836892, 120.282811],
+['Gordon Heights Park', 14.855535, 120.290518],
+['Greenwoods Park', 14.824463, 120.284026]
+];
+
+var schl = [
+['Olongapo City National High School', 14.835157, 120.281872],
+['Gordon Heights Elementary and National High School', 14.856440, 120.290052],
+['Wesley School', 14.831700, 120.280889],
+['Barretto Elementary and High School', 14.856410, 120.270618],
+['Olongapo City Elementary School', 14.844038, 120.289326],
+['Sergia Soriano School', 14.830199, 120.272813],
+['Mabayuan Elementary School', 14.846691, 120.283968],
+['Little Angel Study Center', 14.829406, 120.278562],
+['Balic-Balic Elementary School', 14.858747, 120.292811],
+['Santa Rita Elementary School', 14.849669, 120.293696],
+['Santa Rita High School', 14.850751, 120.299028],
+['Old Cabalan Elementary School', 14.850539, 120.305390],
+['Tabacuhan Elementary School', 14.854652, 120.301132],
+['Kalalake Elementary and High School', 14.829881, 120.288904],
+['Asinan Elementary School', 14.828237, 120.286329],
+['Tapinac Elementary School', 14.832775, 120.283244],
+['James L. Gordon Integrated School', 14.831464, 120.277237],
+['Banicain Elementary School', 14.828602, 120.275869],
+['First School of SBFZ', 14.825025, 120.277730],
+['Regional Science High School', 14.838452, 120.313288]
+];
+
+var col = [
+['The College of Nerd and Wierd People - Gordon College -', 14.836026, 120.283670],
+['Columban College - Barretto', 14.856251, 120.268646],
+['St. Joseph College', 14.837932, 120.285792],
+['Mondrian Aura College', 14.825630, 120.277800],
+['Lyceum of Subic Bay', 14.823450, 120.279109],
+['Columban College - Main', 14.826526, 120.281734],
+['Virgen De Los Remedios College', 14.836140, 120.285473],
+['Holy Infant Jesus College', 14.851028, 120.290381]
+];
+
+var mall = [
+['SM Olongapo', 14.826443, 120.283035],
+['Ayala Harbor Point', 14.824803, 120.280200]
+];
+
+var dptstr = [
+['Savers Appliance Depot', 14.833416, 120.281159],
+['Ocampo', 14.841517, 120.286602],
+['New Star', 14.827597, 120.282747],
+['Centro', 14.839631, 120.284994],
+['Mart One', 14.839474, 120.284843],
+['Savers Appliance Depot', 14.846677, 120.269209]
+];
+
+var grcry = [
+['Circle J', 14.827301, 120.282765],
+['Choas', 14.840163, 120.287170],
+['Vercon', 14.840965, 120.287411],
+['YBC', 14.830190, 120.280355],
+['Vercon', 14.843221, 120.287820],
+['Vercon', 14.841136, 120.286526]
+];
+
+var fstfd = [
+['Jollibee', 14.841027, 120.286439],
+['Jollibee', 14.838543, 120.284377],
+['Jollibee', 14.827066, 120.282441],
+['Jollibee', 14.824214, 120.282202],
+['McDonalds', 14.827189, 120.282817],
+['McDonalds', 14.839867, 120.285216],
+['ChicBoy', 14.827631, 120.282675],
+['Mang Inasal', 14.827328, 120.282505],
+['Greenwich', 14.839067, 120.284308],
+['Chowking', 14.839151, 120.283929]
+];
+
+var bnk = [
+['Philippine National Bank', 14.842770, 120.288887],
+['Land Bank of the Philippines', 14.842713, 120.288659],
+['Metrobank', 14.840067, 120.285432],
+['BPI', 14.839078, 120.284457],
+['RCBC', 14.832666, 120.280583],
+['Union Bank', 14.830094, 120.281034]
+];
+
+var ctyhll = [
+['Olongapo City Hall', 14.842439, 120.287655]
+];
+
+var hsptl = [
+['James L. Gordon Memorial Hospital', 14.826510, 120.280430],
+['Baypointe Hospital and Medical Center', 14.823931, 120.272260],
+['Our Lady of Lourdes International Medical Center', 14.847595, 120.269112]
+];
+
+var gas = [
+['Caltex', 14.845315, 120.290791],
+['Shell', 14.844395, 120.290396],
+['Caltex', 14.835834, 120.281119],
+['Petron', 14.827749, 120.279892],
+['PTT', 14.838440, 120.280021],
+['Flying V', 14.838472, 120.279200]
+];
+
+var pol = [
+['Police Station', 14.842131, 120.287560],
+['Police Camp - 164 -', 14.849124, 120.265741],
+['NBI', 14.838311, 120.278822],
+['Police Station', 14.828950, 120.281727]
+];
+
+var bar = [
+['Gigz Super Club', 14.830872, 120.280821],
+['Gigolo', 14.830830, 120.280869],
+['Ambyanz Nightlife', 14.830723, 120.280986],
+['Tropical Bay', 14.830621, 120.280827],
+['Apozaga Bar', 14.830592, 120.281073],
+['Willis Concert Bar', 14.828692, 120.282043]
+];
+
+var drgstr = [
+['Mercury Drugstore', 14.839162, 120.283577],
+['Mercury Drugstore', 14.841280, 120.286567],
+['Manson', 14.826380, 120.282567]
+];
+
+var mrkt = [
+['Olongapo City Public Market', 14.840602, 120.286877]
+];
+
+var rest = [
+['Maxs Restaurant', 14.827418, 120.282762],
+['Kongs', 14.828350, 120.282300],
+['Sams Pizza', 14.830140, 120.281303],
+['Wimpys', 14.828500, 120.282140]
+];
+
+var cas = [
+['Casino Filipino', 14.828380, 120.282280]
+];
+
+var con = [
+['Olongapo City Convention Center', 14.836670, 120.283002]
+];
+
+function Parks() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < prk.length; i++) {
+var pos = new google.maps.LatLng(prk[i][1], prk[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(prk[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function School() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < schl.length; i++) {
+var pos = new google.maps.LatLng(schl[i][1], schl[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(schl[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function College() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < col.length; i++) {
+var pos = new google.maps.LatLng(col[i][1], col[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(col[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function Mall() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < mall.length; i++) {
+var pos = new google.maps.LatLng(mall[i][1], mall[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(mall[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function DeptStore() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < dptstr.length; i++) {
+var pos = new google.maps.LatLng(dptstr[i][1], dptstr[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(dptstr[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function Grocery() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < grcry.length; i++) {
+var pos = new google.maps.LatLng(grcry[i][1], grcry[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(grcry[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function FastFood() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < fstfd.length; i++) {
+var pos = new google.maps.LatLng(fstfd[i][1], fstfd[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(fstfd[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function Bank() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < bnk.length; i++) {
+var pos = new google.maps.LatLng(bnk[i][1], bnk[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(bnk[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function CityHall() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < ctyhll.length; i++) {
+var pos = new google.maps.LatLng(ctyhll[i][1], ctyhll[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(ctyhll[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function Hospital() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < hsptl.length; i++) {
+var pos = new google.maps.LatLng(hsptl[i][1], hsptl[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(hsptl[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function Gas() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < gas.length; i++) {
+var pos = new google.maps.LatLng(gas[i][1], gas[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(gas[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function Station() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < pol.length; i++) {
+var pos = new google.maps.LatLng(pol[i][1], pol[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(pol[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function Bar() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < bar.length; i++) {
+var pos = new google.maps.LatLng(bar[i][1], bar[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(bar[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function DrugStore() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < drgstr.length; i++) {
+var pos = new google.maps.LatLng(drgstr[i][1], drgstr[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(drgstr[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function Market() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < mrkt.length; i++) {
+var pos = new google.maps.LatLng(mrkt[i][1], mrkt[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(mrkt[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function Restaurant() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < rest.length; i++) {
+var pos = new google.maps.LatLng(rest[i][1], rest[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(rest[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function Casino() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < cas.length; i++) {
+var pos = new google.maps.LatLng(cas[i][1], cas[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(cas[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
+
+//-----------------------------------------------------------------//              //-----------------------------//
+
+function Convention() {
+var myOptions = {
+disableDefaultUI: true,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+mapTypeControl: false
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < con.length; i++) {
+var pos = new google.maps.LatLng(con[i][1], con[i][2]);
+bounds.extend(pos);
+marker = new google.maps.Marker({
+position: pos,
+map: map
+});
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+return function() {
+infowindow.setContent(con[i][0]);
+infowindow.open(map, marker);
+}
+})(marker, i));
+}
+map.fitBounds(bounds);
+}
